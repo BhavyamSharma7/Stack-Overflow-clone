7e64ae068490ed9f30cde4bb475b970a3335e202
BhavyamSharma7 GitHub <bhavyamsharma01@gmail.com>
2023-03-12 20:28:35 +0530

    improvisations

--- server/routes/subscription.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createCheckout } from "../controllers/Subcription.js";
-
-const router = express.Router();
-
-router.post('/create-checkout-session', createCheckout);
-
-export default router;
